7c5f56676c62937d31975f3facebceedace751dc
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2023-02-11 16:54:45 -0600

    update

--- server/src/routes/location.js
+++ server/src/routes/location.js
@@ -12,16 +12,22 @@ location.get('/', (req, res)=>{
 location.post('/location', async(req, res) =>{
     const zipcode = req.body.zipcode;
     const baseUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${apiId}&units=imperial`
-    const weatherdata = await axios.get(baseUrl)
-    .then((response => response.data))
-    res.render('weather', {weather: weatherdata})
-    // console.log(weatherdata)
-    // weatherdata.weather.map((item)=>{
-    //     // console.log(item.main)
-    //     // if (item.main === 'Clouds'){
-    //     //     document.getElementsByClassName('weather-body').style.backgroundImage = 'url("./images/weather-bg.jpg")';
-    //     // }
-    //   })
+        const weatherdata = await axios.get(baseUrl)
+        .then((response => response.data))
+            const name = weatherdata.name
+            const country = weatherdata.sys.country
+            const description = weatherdata.weather[0].description;
+            const main = weatherdata.weather[0].main
+            const icon = weatherdata.weather[0].icon
+            const icon_url = `https://openweathermap.org/img/w/${icon}.png`
+            const feels  = weatherdata.main.feels_like
+            const humidity  = weatherdata.main.humidity
+      
+
+            res.render('weather', {location_name: name, location_country: country, location_description: description, location_main: main, weather_icon: icon_url, location_feels: feels , location_humidity: humidity, zipcode_location: zipcode
+            })
+
+console.log(weatherdata)
 })
 
 
